f4895e01c67eab91a6d93927f02087ef685b0218
Winkel515 <winkel515@hotmail.com>
2021-02-10 20:34:19 -0500

    #8 added a server request in Home.js

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Link} from "react-router-dom";
+import axios from 'axios';
 
 function Home() {
+    const [serverMessage, setServerMessage] = useState('Loading server message...');
+
+    useEffect(() => {
+        async function getServerMessage() {
+            const res = await axios.get('/users');
+            setServerMessage(res.data.message)
+        }
+
+        getServerMessage()
+    },[])
+
     return (
         <div>
             <h1>
@@ -9,6 +21,8 @@ function Home() {
             </h1>
 
             <Link to="/profile">Go to Profile</Link>
+
+            <p>{serverMessage}</p>
         </div>
     )
 }
