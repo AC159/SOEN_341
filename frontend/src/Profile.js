9f218e7499156344e5556e7e4fd1924f75722951
Winkel515 <winkel515@hotmail.com>
2021-02-10 20:21:58 -0500

    #8 Added basic routing

---
+++ frontend/src/Profile.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link} from "react-router-dom";
+
+function Profile() {
+    return (
+        <div>
+            <h1>
+                Profile
+            </h1>
+
+            <Link to="/">Go to Home</Link>
+        </div>
+    )
+}
+
+export default Profile
