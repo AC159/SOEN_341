c0585c4d52449e6a31b81ea87b5a9e62ccb1c220
Anastassy Cap <anacap100@gmail.com>
2021-02-12 11:13:35 -0500

    #8 Connected frontend with backend, we now need to post pictures

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req, res) {
+  res.send({
+    'message': 'Message from server at /users'
+  });
 });
 
 module.exports = router;
