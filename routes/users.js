8698d2c71e1138d61aab80a4320bdfc1863cc1d2
Anastassy Cap <anacap100@gmail.com>
2021-02-13 10:05:04 -0500

    #8 finished connection between frontend and backend

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+
 router.get('/', function(req, res) {
+
   res.send({
-    'message': 'Message from server at /users'
+    'users': ['AC', 'WY', 'NY', 'KL', 'RM']
   });
+
 });
 
 module.exports = router;
