c40116ecff0906ed57f5a20672251963f5de5ee8
Anastassy Cap <anacap100@gmail.com>
2021-02-14 20:06:37 -0500

    can upload image to google storage

---
+++ config/index.js
@@ -0,0 +1,14 @@
+const Cloud = require('@google-cloud/storage');
+const path = require('path');
+const serviceKey = path.join(__dirname, '/google_cloud_api_key.json');
+
+const { Storage } = Cloud;
+
+// Create a client from a google service account key
+
+const storage = new Storage({
+    keyFilename: serviceKey,
+    projectId: 'soen-341-instagram'
+})
+
+module.exports = storage
