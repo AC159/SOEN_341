631ebc68325e03aaac9d3553c1d73bb579ede4aa
Anastassy Cap <anacap100@gmail.com>
2021-02-22 09:37:04 -0500

    Backend can now create a new user

--- Database/Mongoose.js
+++ Database/Mongoose.js
@@ -1,15 +1,15 @@
 let mongoose = require('mongoose');
-let Schema = mongoose.Schema;
-
-let UserSchema = new Schema({
-  id: {type: String, required: true, unique: true},
-  password: {type: String, required: true},
-  images: Array,
-  follows: Array,
-  followers: Array,
-  likes: Array
-})
+const chalk = require('chalk');
+require("dotenv").config({ path: "frontend/.env" })
 
 
-mongoose.connect(process.env.MONGODB_CLUSTER_URL, { useNewUrlParser: true });
-module.exports = mongoose.model('User', UserSchema);
+// This file contains the connection method to connect and save objects to the db
+console.log(process.env.MONGODB_CLUSTER_URL);
+mongoose.connect(process.env.MONGODB_CLUSTER_URL, { useNewUrlParser: true})
+    .then(() => {
+      console.log(chalk.green('Successfully connected to cluster...'))
+    })
+    .catch((error) => {
+      console.log(chalk.red('Unable to connect to cluster...'));
+    });
+
