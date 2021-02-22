bd8b320e454a937509ad561ea62a1446094a51e8
Winkel515 <winkel515@hotmail.com>
2021-02-22 16:37:46 -0500

    change signup route to /users/signup and signup makes a post to the route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+require('./Database/Mongoose.js');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users').router;
