38b5d45352f93478bcf30f1ac45e02d336c87665
Anastassy Cap <anacap100@gmail.com>
2021-03-15 12:14:48 -0400

    #31 users can now see other users followers and the user they are following

---
+++ frontend/src/components/Profile/Followers_Following_Dialog/Dialog.js
@@ -0,0 +1,82 @@
+import React, {useContext} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Dialog from '@material-ui/core/Dialog';
+import PersonIcon from '@material-ui/icons/Person';
+import { blue } from '@material-ui/core/colors';
+import { ModalContext } from '../ModalContextProvider/ModalContextProvider';
+import { useHistory } from "react-router";
+
+const useStyles = makeStyles({
+    avatar: {
+        backgroundColor: blue[100],
+        color: blue[600],
+    },
+});
+
+function SimpleDialog(props) {
+    const classes = useStyles();
+    const { type, onClose, selectedValues, open } = props;
+    const history = useHistory();
+
+    const handleClose = () => {
+        onClose(selectedValues);
+    };
+
+    const handleListItemClick = (user) => {
+        onClose(user);
+        history.push(`/profile/${user._id}`);
+    };
+
+    return (
+        <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open}>
+            <DialogTitle id="simple-dialog-title">{type}</DialogTitle>
+            <List>
+                {selectedValues.map((followerOrfollowing) => (
+                    <ListItem button onClick={() => handleListItemClick(followerOrfollowing)} key={followerOrfollowing._id}>
+                        <ListItemAvatar>
+                            <Avatar className={classes.avatar} src={followerOrfollowing.avatar}>
+                            </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText primary={followerOrfollowing.name} />
+                    </ListItem>
+                ))}
+            </List>
+        </Dialog>
+    );
+}
+
+export default function SimpleDialogDemo(props) {
+    const [open, setOpen] = React.useState(true);
+    const [selectedValues, setSelectedValues] = React.useState(props.data);
+    const { openFollowersModal, openFollowingModal, closeFollowersDialog, closeFollowingDialog } = useContext(ModalContext);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = (value) => {
+        setOpen(false);
+        setSelectedValues(value);
+
+        if (props.type === "followers") {
+            closeFollowersDialog();
+        }
+
+        if (props.type === "following") {
+            closeFollowingDialog();
+        }
+
+    };
+
+    return (
+        <div>
+            <SimpleDialog type={props.type} selectedValues={selectedValues} open={open} onClose={handleClose} />
+        </div>
+    );
+}
